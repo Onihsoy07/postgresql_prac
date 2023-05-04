7b4f02569702c400a6cd3b891e20a54e6da56e4d
Onihsoy07 <pjh2000630@naver.com>
2023-05-04 17:23:29 +0900

    init1

---
+++ src/main/java/com/test/postgresql_test/PostgresqlTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.postgresql_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresqlTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/postgresql_test/PostgresqlTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.postgresql_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresqlTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
