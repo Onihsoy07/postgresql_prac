02c2db9caa7eb65202ebc6c62ce3b0fff795e6be
Onihsoy07 <pjh2000630@naver.com>
2023-06-26 17:18:05 +0900

    싱크로율 퍼센트로 view에 출력

--- src/main/java/com/test/postgresql_test/controller/ExceptionHandlingController.java
+++ src/main/java/com/test/postgresql_test/controller/ExceptionHandlingController.java
@@ -15,51 +15,51 @@ import java.util.Date;
 //@Controller
 public class ExceptionHandlingController /**implements ErrorController*/ {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
-
-    // 에러 페이지 정의
-    private final String ERROR_404_PAGE_PATH = "error/404";
-    private final String ERROR_500_PAGE_PATH = "error/500";
-    private final String ERROR_ETC_PAGE_PATH = "error/error";
-
-    @RequestMapping(value = "/error")
-    public String handleError(HttpServletRequest request, Model model) {
-
-        // 에러 코드를 획득한다.
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-        // 에러 코드에 대한 상태 정보
-        HttpStatus httpStatus = HttpStatus.valueOf(Integer.valueOf(status.toString()));
-
-        System.out.println("error getReasonPhrase: " + httpStatus.getReasonPhrase());
-        System.out.println("error name : " + httpStatus.name());
-
-
-        if (status != null) {
-            // HttpStatus와 비교해 페이지 분기를 나누기 위한 변수
-            int statusCode = Integer.valueOf(status.toString());
-
-            // 로그로 상태값을 기록 및 출력
-            LOGGER.info("httpStatus : " + statusCode);
-
-            // 404 error
-            if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                // 에러 페이지에 표시할 정보
-                model.addAttribute("code", status.toString());
-                model.addAttribute("msg", httpStatus.getReasonPhrase());
-                model.addAttribute("timestamp", new Date());
-                return ERROR_404_PAGE_PATH;
-            }
-
-            // 500 error
-            if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                // 서버에 대한 에러이기 때문에 사용자에게 정보를 제공하지 않는다.
-                return ERROR_500_PAGE_PATH;
-            }
-        }
-
-        // 정의한 에러 외 모든 에러는 error/error 페이지로 보낸다.
-        return ERROR_ETC_PAGE_PATH;
-    }
+//    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+//
+//    // 에러 페이지 정의
+//    private final String ERROR_404_PAGE_PATH = "error/404";
+//    private final String ERROR_500_PAGE_PATH = "error/500";
+//    private final String ERROR_ETC_PAGE_PATH = "error/error";
+//
+//    @RequestMapping(value = "/error")
+//    public String handleError(HttpServletRequest request, Model model) {
+//
+//        // 에러 코드를 획득한다.
+//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+//
+//        // 에러 코드에 대한 상태 정보
+//        HttpStatus httpStatus = HttpStatus.valueOf(Integer.valueOf(status.toString()));
+//
+//        System.out.println("error getReasonPhrase: " + httpStatus.getReasonPhrase());
+//        System.out.println("error name : " + httpStatus.name());
+//
+//
+//        if (status != null) {
+//            // HttpStatus와 비교해 페이지 분기를 나누기 위한 변수
+//            int statusCode = Integer.valueOf(status.toString());
+//
+//            // 로그로 상태값을 기록 및 출력
+//            LOGGER.info("httpStatus : " + statusCode);
+//
+//            // 404 error
+//            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+//                // 에러 페이지에 표시할 정보
+//                model.addAttribute("code", status.toString());
+//                model.addAttribute("msg", httpStatus.getReasonPhrase());
+//                model.addAttribute("timestamp", new Date());
+//                return ERROR_404_PAGE_PATH;
+//            }
+//
+//            // 500 error
+//            if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+//                // 서버에 대한 에러이기 때문에 사용자에게 정보를 제공하지 않는다.
+//                return ERROR_500_PAGE_PATH;
+//            }
+//        }
+//
+//        // 정의한 에러 외 모든 에러는 error/error 페이지로 보낸다.
+//        return ERROR_ETC_PAGE_PATH;
+//    }
 
 }
--- src/main/java/com/test/postgresql_test/controller/IndexController.java
+++ src/main/java/com/test/postgresql_test/controller/IndexController.java
@@ -36,7 +36,7 @@ public class IndexController {
     @GetMapping({"/",""})
     public String index(Model model,
                         @PageableDefault(size = 15, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
-        model.addAttribute("state", "");
+        model.addAttribute("state", "/?");
         model.addAttribute("boards", boardService.boardList(cusPageable(pageable)));
         model.addAttribute("topRateCfr", cfrDataRepository.findTop10ByOrderByConfidenceDesc());
         return "index";
@@ -49,7 +49,7 @@ public class IndexController {
                             HttpServletRequest request,
                             HttpServletResponse response) {
 //        response = boardService.viewCount(id, request, response);
-        model.addAttribute("state", "");
+        model.addAttribute("state", "/?");
         model.addAttribute("replyList", replyRepository.findByBoard_IdOrderByCreateDateAsc(id));
         model.addAttribute("topRateCfr", cfrDataRepository.findTop10ByOrderByConfidenceDesc());
         model.addAttribute("boardView", boardService.findById(id));
